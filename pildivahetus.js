b2c512549bcfeabc8a4458f0416a8cdc88cc707c
PeeterPulber <uku.viispert@gmail.com>
2023-12-03 12:43:15 +0200

    Parandasin js vea

--- pildivahetus.js
+++ pildivahetus.js
@@ -1,7 +1,9 @@
+//Allikas: Uku Viispert ChatGPT abiga
+
 const pildielement = document.getElementById("muutuv");
         
 function muuda_pilti() {
-    const suvanumber = Math.floor(Math.random() * 31) + 1
+    const suvanumber = Math.floor(Math.random() * 31) + 1;
     const pilt = "pildid/grupipilt/" + suvanumber + ".JPG";
     pildielement.src = pilt;
 };
